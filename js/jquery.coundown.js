0f84038d7e138d0ae85e1e4ded6d512d30eedb5a
wolix22500 <144556466+wolix22500@users.noreply.github.com>
2025-01-06 22:01:02 +0100

    Update jquery.coundown.js

--- js/jquery.coundown.js
+++ js/jquery.coundown.js
@@ -57,7 +57,7 @@
 				current_date = currentDate(); // get fixed current date
 
 			// difference of dates
-			var difference = target_date - current_date;
+			var difference = current_date - target_date;
 
 			// if difference is negative than it's pass the target date
 			if (difference < 0) {
